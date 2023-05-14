d55b5b80dfcc7f0564447fc46dc3b81eac852b19
Alessandro Gelsi <79928745+Alegelx24@users.noreply.github.com>
2023-05-14 19:46:11 +0200

    not working plot discord matrix profile

--- stumpy_discord.py
+++ stumpy_discord.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import stumpy
 import numpy as np
@@ -5,8 +6,6 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as dates
 from matplotlib.patches import Rectangle
 import datetime as dt
-
-
 
 
 steam_df = pd.read_csv("https://zenodo.org/record/4273921/files/STUMPY_Basics_steamgen.csv?download=1")
@@ -25,15 +24,19 @@ mp = stumpy.stump(steam_df['steam flow'], m)
 
 discord_idx = np.argsort(mp[:, 0])[-1]
 
+
 print(f"The discord is located at index {discord_idx}")
 
+'''
 nearest_neighbor_distance = mp[discord_idx, 0]
 
 print(f"The nearest neighbor subsequence to this discord is {nearest_neighbor_distance} units away")
+'''
 
 fig, axs = plt.subplots(2, sharex=True, gridspec_kw={'hspace': 0})
 plt.suptitle('Discord (Anomaly/Novelty) Discovery', fontsize='30')
 
+'''
 axs[0].plot(steam_df['steam flow'].values)
 axs[0].set_ylabel('Steam Flow', fontsize='20')
 rect = Rectangle((discord_idx, 0), m, 40, facecolor='lightgrey')
@@ -45,7 +48,6 @@ axs[1].plot(mp[:, 0])
 plt.show()
 
 
-'''
 taxi_df = pd.read_csv("https://zenodo.org/record/4276428/files/STUMPY_Basics_Taxi.csv?download=1")
 taxi_df['value'] = taxi_df['value'].astype(np.float64)
 taxi_df['timestamp'] = pd.to_datetime(taxi_df['timestamp'], format='%Y-%m-%d %H:%M:%S')
