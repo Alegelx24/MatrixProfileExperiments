31c525b2ad608d11f6cfd5ad8f904ee621e3d81a
Alessandro Gelsi <79928745+Alegelx24@users.noreply.github.com>
2023-05-14 12:34:06 +0200

    feat: added working discord finder test

---
+++ stumpy_discord.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import stumpy
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as dates
+from matplotlib.patches import Rectangle
+import datetime as dt
+
+
+
+
+steam_df = pd.read_csv("https://zenodo.org/record/4273921/files/STUMPY_Basics_steamgen.csv?download=1")
+#steam_df = pd.read_csv("C:/Users/aleg2/Desktop/MatrixProfileExperiments/BourkeStreetMall.txt")
+
+steam_df.head()
+
+plt.suptitle('Steamgen Dataset', fontsize='30')
+plt.xlabel('Time', fontsize ='20')
+plt.ylabel('Steam Flow', fontsize='20')
+plt.plot(steam_df['steam flow'].values)
+plt.show()
+
+m = 640
+mp = stumpy.stump(steam_df['steam flow'], m)
+
+discord_idx = np.argsort(mp[:, 0])[-1]
+
+print(f"The discord is located at index {discord_idx}")
+
+nearest_neighbor_distance = mp[discord_idx, 0]
+
+print(f"The nearest neighbor subsequence to this discord is {nearest_neighbor_distance} units away")
+
+fig, axs = plt.subplots(2, sharex=True, gridspec_kw={'hspace': 0})
+plt.suptitle('Discord (Anomaly/Novelty) Discovery', fontsize='30')
+
+axs[0].plot(steam_df['steam flow'].values)
+axs[0].set_ylabel('Steam Flow', fontsize='20')
+rect = Rectangle((discord_idx, 0), m, 40, facecolor='lightgrey')
+axs[0].add_patch(rect)
+axs[1].set_xlabel('Time', fontsize ='20')
+axs[1].set_ylabel('Matrix Profile', fontsize='20')
+axs[1].axvline(x=discord_idx, linestyle="dashed")
+axs[1].plot(mp[:, 0])
+plt.show()
+
+
+'''
+taxi_df = pd.read_csv("https://zenodo.org/record/4276428/files/STUMPY_Basics_Taxi.csv?download=1")
+taxi_df['value'] = taxi_df['value'].astype(np.float64)
+taxi_df['timestamp'] = pd.to_datetime(taxi_df['timestamp'], format='%Y-%m-%d %H:%M:%S')
+taxi_df.head()
+
+# This code is going to be utilized to control the axis labeling of the plots
+DAY_MULTIPLIER = 7  # Specify for the amount of days you want between each labeled x-axis tick
+
+x_axis_labels = taxi_df[(taxi_df.timestamp.dt.hour==0)]['timestamp'].dt.strftime('%b %d').values[::DAY_MULTIPLIER]
+x_axis_labels[1::2] = " "
+x_axis_labels, DAY_MULTIPLIER
+
+plt.suptitle('Taxi Passenger Raw Data', fontsize='30')
+plt.xlabel('Window Start Date', fontsize ='20')
+plt.ylabel('Half-Hourly Average\nNumber of Taxi Passengers', fontsize='20')
+plt.plot(taxi_df['value'])
+
+plt.xticks(np.arange(0, taxi_df['value'].shape[0], (48*DAY_MULTIPLIER)/2), x_axis_labels)
+plt.xticks(rotation=75)
+plt.minorticks_on()
+plt.margins(x=0)
+plt.show()
+
+m = 48
+mp = stumpy.gpu_stump(taxi_df['value'], m=m)
+
+
+#VISUALIZE THE MATRIX PROFILE HERE
+plt.suptitle('1-Day STUMP', fontsize='30')
+plt.xlabel('Window Start', fontsize ='20')
+plt.ylabel('Matrix Profile', fontsize='20')
+plt.plot(mp[:, 0])
+
+plt.plot(575, 1.7, marker="v", markersize=15, color='b')
+plt.text(620, 1.6, 'Columbus Day', color="black", fontsize=20)
+plt.plot(1535, 3.7, marker="v", markersize=15, color='b')
+plt.text(1580, 3.6, 'Daylight Savings', color="black", fontsize=20)
+plt.plot(2700, 3.1, marker="v", markersize=15, color='b')
+plt.text(2745, 3.0, 'Thanksgiving', color="black", fontsize=20)
+plt.plot(30, .2, marker="^", markersize=15, color='b', fillstyle='none')
+plt.plot(363, .2, marker="^", markersize=15, color='b', fillstyle='none')
+plt.xticks(np.arange(0, 3553, (m*DAY_MULTIPLIER)/2), x_axis_labels)
+plt.xticks(rotation=75)
+plt.minorticks_on()
+plt.show()
+
+#STUMP WITH DIFFERENT WINDOWS SIZING
+days_dict ={
+  "Half-Day": 24,
+  "1-Day": 48,
+  "2-Days": 96,
+  "5-Days": 240,
+  "7-Days": 336,
+}
+
+days_df = pd.DataFrame.from_dict(days_dict, orient='index', columns=['m'])
+days_df.head()
+
+fig, axs = plt.subplots(5, sharex=True, gridspec_kw={'hspace': 0})
+fig.text(0.5, -0.1, 'Subsequence Start Date', ha='center', fontsize='20')
+fig.text(0.08, 0.5, 'Matrix Profile', va='center', rotation='vertical', fontsize='20')
+for i, varying_m in enumerate(days_df['m'].values):
+    mp = stumpy.stump(taxi_df['value'], varying_m)
+    axs[i].plot(mp[:, 0])
+    axs[i].set_ylim(0,9.5)
+    axs[i].set_xlim(0,3600)
+    title = f"m = {varying_m}"
+    axs[i].set_title(title, fontsize=20, y=.5)
+plt.xticks(np.arange(0, taxi_df.shape[0], (48*DAY_MULTIPLIER)/2), x_axis_labels)
+plt.xticks(rotation=75)
+plt.suptitle('STUMP with Varying Window Sizes', fontsize='30')
+plt.show()
+'''
